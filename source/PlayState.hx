07d5efbb83feb222d84d72049f0dc20b2fa820bb
Jon Borgonia <jon@gomagames.com>
2016-05-05 14:13:00 -1000

    add Map sprite

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -6,12 +6,17 @@ import flixel.FlxState;
 import flixel.text.FlxText;
 import flixel.ui.FlxButton;
 import flixel.math.FlxMath;
+import sprites.Map;
 
 class PlayState extends FlxState
 {
+  private var map:Map;
+
 	override public function create():Void
 	{
 		super.create();
+    map = new Map(this);
+    add(map);
 	}
 
 	override public function update(elapsed:Float):Void
---
+++ source/sprites/Map.hx
@@ -0,0 +1,11 @@
+package sprites;
+
+import flixel.FlxSprite;
+import flixel.util.FlxColor;
+
+class Map extends FlxSprite {
+
+  public function new(state:PlayState) {
+    super(0,0);
+  }
+}
