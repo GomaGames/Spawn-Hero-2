ea0d5dff677a5e3ba77ca3a7a3eafc4f5a268432
Jon Borgonia <jon@gomagames.com>
2016-05-06 10:43:17 -1000

    add MenuState delay and countdown

--- source/MenuState.hx
+++ source/MenuState.hx
@@ -7,9 +7,17 @@ import flixel.text.FlxText;
 import flixel.ui.FlxButton;
 import flixel.math.FlxMath;
 import flixel.util.FlxColor;
+import flixel.util.FlxTimer;
 
 class MenuState extends FlxState
 {
+  private var continue_delay:Int = 2; // don't allow rapid continue while holding buttons
+  private var allow_continue:Bool; // don't allow rapid continue while holding buttons
+  private var continue_timer:FlxTimer; // don't allow rapid continue while holding buttons
+  private var ready_text:FlxText; // don't allow rapid continue while holding buttons
+  private var countdown_timer:FlxTimer;
+  private var countdown_seconds:Int = 3;
+
 	override public function create():Void
 	{
 		super.create();
@@ -21,11 +29,36 @@ class MenuState extends FlxState
     titleImage.screenCenter();
     add( titleImage );
 
+    continue_timer = new FlxTimer();
+    continue_timer.start(continue_delay, function(t){
+      allow_continue = true;
+
+      ready_text = new FlxText( 400, 600, Std.string( "READY?" ) );
+      ready_text.setFormat( "Arial", 30, Main.FONT_BLUE, FlxTextAlign.CENTER, FlxTextBorderStyle.SHADOW, FlxColor.BLACK, true);
+      add( ready_text );
+
+    });
+
 	}
+
+  private inline function start_countdown():Void
+  {
+    countdown_timer = new FlxTimer();
+    countdown_timer.start(countdown_seconds, function(t){
+      FlxG.switchState( new PlayState() );
+    });
+  }
 
 	override public function update(elapsed:Float):Void
 	{
-    if( FlxG.keys.getIsDown().length > 0 ) FlxG.switchState( new PlayState() );
+    if( allow_continue && countdown_timer == null && FlxG.keys.getIsDown().length > 0 ){
+      start_countdown();
+    }
+
+    if( countdown_timer != null ){
+      ready_text.text = Std.string(Std.int(countdown_timer.timeLeft));
+    }
+
 		super.update(elapsed);
 	}
 }
